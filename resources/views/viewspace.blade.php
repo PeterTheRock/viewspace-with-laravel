1f1ddbe955c14a20163fa279778170a56c5efe12
Peter Hendricks <Peterhendricks1997@gmail.com>
2022-08-10 20:55:34 +0200

    first commit

---
+++ resources/views/viewspace.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.layout')
+<!--below must be difined as a section of content-->
+@section('sectionone')<!--Section we want to dump into our layouts.php-->
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    ViewSpace </br>
+                   <h6>Express Your Thoughts</h6>
+                </div>
+
+                <div class="links">
+                    <!--
+                        <p>{{ $internet}} <br> {{$type}}<br>{{$base}}</p>
+                    These variables above are not being used if not omitted can cause problems for laravel
+                    -->
+                   
+                   
+                        <!--@for($i =0; $i < 5; $i++)
+                   <p>The value of i is {{$i}}</p>
+                   @endfor-->
+                   <!--Creation of a forloop is VERY IMPORTANT-->
+
+                   <!-- @for($i = 0;$i < count($internet); $i++)
+                   <p>{{$viewspaces[$i]['type']}}</p>
+                   @endfor -->
+                
+                   @foreach($internet -> as $comment)
+                   <div>{{$loop->index}} {{$comment['type']}} - {{$comment['base']}}
+                    
+
+                    @if($loop->first)
+                    <span>- First in the loop</span>
+                    @endif
+                   </div>
+                   @endforeach
+                 
+                </div> The code in the loop is not working for some reason, going to keep moving on
+            </div>
+            <!--allows us to yield a specified '@Section' all content from a section is kept here after Laravel processing-->@yield('sectionone')
+        </div>
+        @endsection
+    
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Code below points to the views folder welcome page
+Route::get('/viewspace', function () {
+    return view('viewspace');
+});
+
+//Code below is an array for the views folder
+Route::get('/viewspace', function () {
+   
+        $dev=['dev'=>'Built using Laravel'];
+        $internet=[
+            ['type'=>'Internet','base'=>'Chat'],
+            ['type'=>'Cloud','base'=>'Send Email'],
+            ['type'=>'Networking','base'=>'Check online friends']
+        ];
+    return view('viewspace', ['internet'=>$internet]);
+});
+
